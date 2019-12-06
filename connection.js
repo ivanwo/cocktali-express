4746c3d778cd4fac60391b02ba74deadce294d02
reuelethan <reuelbeats@gmail.com>
2019-12-06 10:16:00 -0500

    started in connections

--- connection.js
+++ connection.js
@@ -1,6 +1,14 @@
-// TO DO:
-const { Pool } = require("pg");
-const pool = new Pool({
-  //TO DO: set up connection to DB
-  //        place relevant credentials here
-});
+  const {
+    Pool
+  } = require('pg');
+  //const connectionString = 'postgresql://postgres:david-password@localhost:5432/postgres';
+  const pool = new Pool({
+    user: "postgres",
+    password: "david-password",
+    host: "localhost",
+    port: 5432,
+    database: "postgres",
+    ssl: false
+  });
+
+  module.exports = pool;
