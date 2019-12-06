d204d1d852bcefbc1a5d08e0a181053bf90c9139
ivan <ivanwolyniec@gmail.com>
2019-12-06 11:35:06 -0500

    gets all user info and publicly displays

--- routes.js
+++ routes.js
@@ -7,21 +7,23 @@ const cocktaliRoutes = express.Router();
 
 cocktaliRoutes.get("/login", (req, res) => {
   // If the request has a ?name= parameter, only respond w/ matching students
-  if (req.query.name) {
-    const sql = "SELECT * FROM cocktali_express WHERE name LIKE $1::TEXT";
-    const params = ["%" + req.query.name + "%"];
-    pool.query(sql, params).then(result => {
-      // .json sends response as send Status "Success"
-      res.status("Success"); // ❌❌IVAN DOUBLE CHECK THIS❌❌
-    });
-  } else {
-    // else respond with ALL students.
-    const sql = "SELECT * FROM cocktali_express";
-    pool.query(sql).then(result => {
-      // .json sends response as status Failure
-      res.status("404") //❌❌IVAN DOUBLE CHECK THIS❌❌
-    });
-  }
+  // if (req.query.name) {
+  //   const sql = "SELECT * FROM cocktali_user";
+  //   // const params = ["%" + req.query.name + "%"];
+  //   pool.query(sql, params).then(result => {
+  //     // .json sends response as send Status "Success"
+  //     res.status(200); // ❌❌IVAN DOUBLE CHECK THIS❌❌
+  //     res.send(result.rows);
+  //   });
+  // } else {
+  //   // else respond with ALL students.
+  const sql = "SELECT * FROM cocktali_user";
+  pool.query(sql).then(result => {
+    // .json sends response as status Failure
+    res.status("200"); //❌❌IVAN DOUBLE CHECK THIS❌❌
+    res.send(result.rows);
+  });
+  // }
   // TO DO: check if email/ pw from req matched db contents
   // yes: return unique user ID associated
   // no: return null
