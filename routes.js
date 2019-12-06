5a1a0515cc1bbda6917eee6bc5b97ac2a94254f0
kenneyaisho <kenneyaisho@gmail.com>
2019-12-06 14:43:55 -0500

    added notes methods in routes js file

--- routes.js
+++ routes.js
@@ -4,6 +4,7 @@ const express = require("express");
 // based on if we're conducting local DB activity vs cloud activity
 const pool = require("./connection");
 const cocktaliRoutes = express.Router();
+const notes = express.Router();
 
 cocktaliRoutes.get("/login", (req, res) => {
   // If the request has a ?name= parameter, only respond w/ matching students
@@ -38,4 +39,18 @@ cocktaliRoutes.get("/addnote", (req, res) => {
   // yes: return something
   // no: return something
 });
+notes.get('/notes', (req, res) => {
+  const notesSql = "SELECT * FROM notes";
+  pool.query(notesSql).then(result => {
+    res.status('2oo');
+    res.send(result);
+  })
+});
+
+notes.post('/notes', (req, res) => {});
+
+notes.delete('/notes', (req, res) => {});
+
+notes.put('/notes', (req, res) => {});
+
 module.exports = cocktaliRoutes;
