7221b06dc28935076debf6a1c8e5f97d11d05995
reuelethan <reuelbeats@gmail.com>
2019-12-06 10:04:22 -0500

    First SQL

---
+++ init-db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE cocktali_user (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255)
+);
+
+INSERT INTO cocktali_user (name, email, password)
+VALUES ('Ivan', 'ivan@ivan.me', 'ivanpassword');
+INSERT INTO cocktali_user (name, email, password)
+VALUES ('Reu', 'reu@reu.me', 'reupassword');
+INSERT INTO cocktali_user (name, email, password)
+VALUES ('Kenny', 'kenny@kenny.me', 'kennypassword');
