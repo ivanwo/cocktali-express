68be14912f62fc67ea7ee419bf5ddb6eb3d69f65
reuelethan <reuelbeats@gmail.com>
2019-12-09 10:04:38 -0500

    added Date

--- notes-table.sql
+++ notes-table.sql
@@ -3,7 +3,7 @@ CREATE TABLE notes_table
 (
   id SERIAL PRIMARY KEY,
   pinned BOOLEAN,
-  added BOOLEAN,
+  added DATE,
   title VARCHAR(255),
   content VARCHAR(255),
   userID INT
@@ -12,4 +12,6 @@ CREATE TABLE notes_table
 INSERT INTO notes_table
   (pinned, added, title, content, userID)
 VALUES
-  (TRUE, TRUE, 'Zoink', 'Drink is good', 12);
+  (TRUE, '1000-01-01', 'Zoink', 'Drink is good', 12);
+
+-- The DATE type is used for values with a date part but no time part. MySQL retrieves and displays DATE values in 'YYYY-MM-DD' format. The supported range is '1000-01-01' to '9999-12-31'.
--- routes.js
+++ routes.js
@@ -1,48 +1,48 @@
-const express = require('express');
+const express = require("express");
 //test
 // note: the pool requirement will have to change
 // based on if we're conducting local DB activity vs cloud activity
-const pool = require('./connection');
+const pool = require("./connection");
 const cocktaliRoutes = express.Router();
 const notes = express.Router();
 
-cocktaliRoutes.get('/login', (req, res) => {
-  const sql = 'SELECT * FROM cocktali_user';
+cocktaliRoutes.get("/login", (req, res) => {
+  const sql = "SELECT * FROM cocktali_user";
   pool.query(sql).then(result => {
-    res.status('200');
+    res.status("200");
     res.send(result.rows);
   });
   // TO DO: check if email/ pw from req matched db contents
   // yes: return unique user ID associated
   // no: return null
 });
-cocktaliRoutes.get('/signup', (req, res) => {
+cocktaliRoutes.get("/signup", (req, res) => {
   // TO DO: send name, email, password
   // yes: if email is not already in DB, return new unique user ID
   // no: return null
 });
-cocktaliRoutes.get('/addnote', (req, res) => {
+cocktaliRoutes.get("/addnote", (req, res) => {
   // TO DO: POST note to notes table
   // yes: return something
   // no: return something
 });
-cocktaliRoutes.get('/notes', (req, res) => {
-  const notesSql = 'SELECT * FROM notes_table';
+cocktaliRoutes.get("/notes", (req, res) => {
+  const notesSql = "SELECT * FROM notes_table";
   pool.query(notesSql).then(result => {
     if (result.rows.length === 0) {
       res.status(404);
     } else {
-      res.status('200');
+      res.status("200");
       res.send(result.rows);
     }
   });
 });
 
-cocktaliRoutes.post('/notes', (req, res) => {
+cocktaliRoutes.post("/notes", (req, res) => {
   const note = req.body;
 
   const notesSql = `INSERT INTO notes_table (pinned, added, title, content, userID) 
-VALUES ($1::BOOLEAN, $2::BOOLEAN, $3::VARCHAR, $4::VARCHAR, $5::INT) RETURNING*`;
+VALUES ($1::BOOLEAN, $2::DATE, $3::VARCHAR, $4::VARCHAR, $5::INT) RETURNING*`;
 
   const params = [
     note.pinned,
@@ -58,7 +58,7 @@ VALUES ($1::BOOLEAN, $2::BOOLEAN, $3::VARCHAR, $4::VARCHAR, $5::INT) RETURNING*`
   });
 });
 
-cocktaliRoutes.delete('/notes/:id', (req, res) => {
+cocktaliRoutes.delete("/notes/:id", (req, res) => {
   const id = parseInt(req.params.id);
 
   const notesSql = `DELETE FROM notes_table WHERE id = $1::INT`;
@@ -70,12 +70,12 @@ cocktaliRoutes.delete('/notes/:id', (req, res) => {
   });
 });
 
-cocktaliRoutes.put('/notes/:id', (req, res) => {
+cocktaliRoutes.put("/notes/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const note = req.body;
 
   const notesSql = `UPDATE notes_table 
-SET pinned = $1::BOOLEAN, added = $2::BOOLEAN, title = $3::VARCHAR, content = $4::VARCHAR, userID = $5::INT
+SET pinned = $1::BOOLEAN, added = $2::DATE, title = $3::VARCHAR, content = $4::VARCHAR, userID = $5::INT
 WHERE ID = $6::INT RETURNING *`;
 
   const params = [
